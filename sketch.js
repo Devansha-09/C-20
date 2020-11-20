e46944f93db34b48b1e619acc525ed0a9af5d64a
Devansha-09 <74242812+Devansha-09@users.noreply.github.com>
2020-11-20 17:13:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+
+var moving_rect, fixed_rect;
+
+function setup() {
+  createCanvas(800,400);
+  fixed_rect=createSprite(400,200,50,80);
+  fixed_rect.shapeColor="green";
+
+  moving_rect=createSprite(400,200,80,30);
+  moving_rect.shapeColor="green";
+
+}
+
+function draw() {
+  background(255,255,255);  
+  console.log(moving_rect.x-fixed_rect.x);
+
+  moving_rect.x=World.mouseX;
+  moving_rect.y=World.mouseY;
+
+  if(moving_rect.x - fixed_rect.x<fixed_rect.width/2+moving_rect.width/2
+     && fixed_rect.x - moving_rect.x<fixed_rect.width/2+moving_rect.width/2
+     && moving_rect.y - fixed_rect.y<fixed_rect.height/2+moving_rect.height/2
+     && fixed_rect.y - moving_rect.y<fixed_rect.height/2+moving_rect.height/2) {
+      fixed_rect.shapeColor = "red";
+      moving_rect.shapeColor = "red";
+    }
+   else{
+    fixed_rect.shapeColor = "green";
+    moving_rect.shapeColor = "green";
+   }
+  drawSprites();
+}
